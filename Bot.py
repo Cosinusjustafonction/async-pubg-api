9dd295d74bc6b3cb45553a50a3c6fafa7c61efc8
Cosinusjustafonction <64425437+Cosinusjustafonction@users.noreply.github.com>
2021-01-13 19:50:45 -0600

    adding id_fromm_username
    now you can get the account id just by typing the username

--- Bot.py
+++ Bot.py
@@ -1,15 +1,16 @@
 import aiohttp
-
-import aiohttp
 import asyncio
-class Bot:
+class pypubgi(object):
+
     def __init__(self,token):
         self.token = token
         self.header = {"Accept": "application/vnd.api+json", "Authorization": f"Bearer {token}"}
+    
     async def request(self, query):
         async with aiohttp.ClientSession(headers=self.header) as session:
             async with session.get(query) as response:
                 return await response.json()
+    
     async def current_seasons(self ):
         url = "https://api.pubg.com/shards/steam/seasons"
         dictionary = await self.request(url)
@@ -23,3 +24,13 @@ class Bot:
             season = e
 
         return season
+    
+
+    #async def stats(self , season , account_id) : 
+        #url = "https://api.pubg.com/shards/steam/players/{}/seasons/{}".format(account_id , season)
+    
+    
+    async def id_from_username(self , username) : 
+        url = "https://api.pubg.com/shards/steam/players?filter[playerNames]={}".format(username)
+        _id = dict(await self.request(url)).get("data")[0].get("id")
+        return _id
